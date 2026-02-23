fc09df072efb10bc80640905ae0ff74905fa5f75
Nishan Kumar Prusty <119807644+Nishan1546@users.noreply.github.com>
2026-02-24 02:19:52 +0530

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,316 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Movie Recommended System",
+    description:
+      "A content-based movie recommender system that suggests top-5 similar movies using Python, Scikit-learn, and TMDB datasets through an interactive web interface.",
+    subDescription: [
+      " Built a content-based movie recommender system using Kaggle datasets (TMDB 5000 Movies & Credits) with Python, Pandas, and Scikit-learn.", 
+      "Engineered features from genres, keywords, cast, and crew, and applied CountVectorizer + Cosine Similarity to generate top-5 personalized recommendations. ",
+      "Developed an interactive web interface in PyCharm to allow users to select a movie and instantly view recommendations",
+    ],
+    href: "https://github.com/Nishan1546/Machine-Learning",
+    logo: "/assets/logos/mrslogo.png",
+    image: "/assets/projects/accessories.png",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/python.png",
+      },
+      {
+        id: 2,
+        name: "Kaggle",
+        path: "/assets/logos/kaggle.png",
+      },
+      {
+        id: 3,
+        name: "PyCharm",
+        path: "/assets/logos/PyCharm.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Weather Forecasting- Dashboard",
+    description:
+      "An interactive Power BI weather forecasting dashboard that visualizes real-time temperature, air quality, humidity, wind speed, and rainfall probabilities, providing clear insights into local and weekly weather trends.",
+    subDescription: [
+      "Built an interactive Power BI dashboard to visualize real-time weather patterns and forecasts.",
+"Integrated multiple datasets for trend analysis and predictive insights.",
+" Enhanced decision-making by enabling temperature, humidity, and rainfall tracking across regions.",
+    ],
+    href: "https://github.com/Nishan1546/Power-Bi/blob/main/Screenshot%202025-08-18%20135704.png",
+    logo: "",
+    image: "/assets/projects/weather2.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "Weather API",
+        path: "/assets/logos/wapi.png",
+      },
+      {
+        id: 2,
+        name: "PowerBi",
+        path: "/assets/logos/powerbi.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Spotify Music Analysis-Report 1",
+    description:
+      "Designed 2 dashboards to analyze and visualize trending songs, artists, and genres. ",
+    subDescription: [
+      "Visualizations track the current trajectory of songs, artists, and genres, identifying what's rising and falling in popularity.",
+  
+      "The dashboard leverages a Seaborn-style heatmap to track daily/monthly streaming activity and a Matplotlib/Plotly donut chart to represent the $36\%$ average 'Energy $\%$' of the music streamed.",
+    ],
+    href: "https://github.com/Nishan1546/Power-Bi/blob/main/Screenshot%202025-08-26%20214113.png",
+    logo: "",
+    image: "/assets/projects/spotify1.png",
+    tags: [
+      {
+        id: 1,
+        name: "Python",
+        path: "/assets/logos/python.png",
+      },
+      {
+        id: 2,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 3,
+        name: "PowerBi",
+        path: "/assets/logos/powerbi.png",
+      },
+      {
+        id: 4,
+        name: "Kaggle",
+        path: "/assets/logos/kaggle.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Spotify Music Analysis Report-2",
+    description:
+      "This dashboard presents a high-level, historical summary of music performance by aggregating total stream data. Key features include:",
+    subDescription: [
+      "Total KPIs like Total Tracks ($\text{924}$), Total Streams ($\text{488bn}$), and Avg Streams ($\text{528.11M}$) across the entire analyzed dataset.",
+      "A line chart visualizes Streams by Release Date from 1900 to 2020, showing historical trends & peaks, while the dashboard also includes a detailed Track & Stream by Month table & a music info carousel shows the Top 10 Streamed Music.",
+      
+      
+    ],
+    href: "https://github.com/Nishan1546/Power-Bi/blob/main/Screenshot%202025-08-26%20214003.png",
+    logo: "",
+    image: "/assets/projects/spotify2.png",
+    tags: [
+      {
+        id: 1,
+        name: "PowerBi",
+        path: "/assets/logos/powerbi.png",
+      },
+      {
+        id: 2,
+        name: "Kaggle",
+        path: "/assets/logos/kaggle.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Game Engine Development",
+    description:
+      "A lightweight C++ game engine designed for 2D and 3D game development.",
+    subDescription: [
+      "Built a powerful rendering engine using OpenGL and C++.",
+      "Developed a physics engine with collision detection and particle effects.",
+      "Implemented a scripting system for easy game customization.",
+      "Optimized performance with multi-threading and efficient memory management.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "C++",
+        path: "/assets/logos/cplusplus.svg",
+      },
+      {
+        id: 2,
+        name: "C#",
+        path: "/assets/logos/csharp.svg",
+      },
+      {
+        id: 3,
+        name: "Git",
+        path: "/assets/logos/git.svg",
+      },
+      {
+        id: 4,
+        name: "Microsoft",
+        path: "/assets/logos/microsoft.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "3D Portfolio Website",
+    description:
+      "This portfolio is an engaging, modern professional showcase, built around a visually stunning, 3D interactive front page with a surreal, cosmic theme.",
+    subDescription: [
+      "The journey begins with a dynamic 3D animation of an astronaut falling, serving as an imaginative introduction to the analyst's unique approach.",
+      " The navigation follows a clean professional hierarchy, moving from the About Me story to the evidence-based Project Section (featuring complex dashboards like the Spotify Analysis), the Work Experience Section, client Testimonials, and concluding with a clear Contact Section.",
+     
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/portfolio.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Tailwind CSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 3,
+        name: "Vite.js",
+        path: "/assets/logos/vitejs.svg",
+      },
+      {
+        id: 4,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/nishan-kumar-prusty",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/_.n.i.s.h.a.n._17/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  
+  {
+    title: "Introduction to Career Skills in Data Analytics",
+    job: "LinkedIn Learning x Microsoft",
+    date: "July 2024-August 2024",
+    contents: [
+      "Learned essential career skills for data analytics roles, including communication, teamwork, and problem-solving.",
+      "Gained insights into the data analytics industry and best practices for career development.",
+      "✅Developed strong communication skills to effectively present data findings to non-technical stakeholders.",
+      "✅Learned teamwork strategies for collaborating on data projects in cross-functional teams.",
+      "✅Enhanced problem-solving abilities by applying analytical thinking to real-world  data scenarios.",
+    ],
+  },
+  {
+    title: "Complete Data Analysis Bootcamp",
+    job: "Udemy",
+    date: "July 2025-Ongoing",
+    contents: [
+      "Gained expertise in Python, SQL, Excel, Power BI, Tableau, AWS, and Snowflake for complete data analysis.",
+      "Built end-to-end data workflows and dashboards to transform raw data into actionable business insights.",
+      "✅Mastered key data tools including Python, SQL, Excel, Power BI, Tableau, AWS, and Snowflake.",
+      "✅Gained hands-on experience with real-world datasets, enhancing data cleaning, analysis, and visualization skills.",
+      "✅Completed projects involving data manipulation, statistical analysis, and dashboard creation to showcase practical applications.",
+
+    ],
+  },
+  {
+    title: "Race to Certification 2025 (Data Science) ",
+    job: "Oracle",
+    date: "September 2025-Ongoing",
+    contents: [
+      "An Oracle-led certification program focused on mastering the Machine Learning Lifecycle and MLOps practices.",
+      "The course covers Introduction & Configuration, Workspace Design, and ML deployment workflows, preparing learners for real-world data science applications.",
+      "✅Learned the end-to-end Machine Learning Lifecycle, including data preparation, model training, evaluation, and deployment.",
+      "✅Gained hands-on experience with MLOPs practices, including model versioning, monitoring, and retraining strategies.",
+      "✅Developed skills in designing scalable ML workflows and integrating ML models into production environments.",
+    ],
+  },
+  {
+    title: "Accelerate Data Science Workflows with Zero Code Changes ",
+    job: "NVDIA",
+    date: "September 2025-September 2025",
+    contents: [
+      "Completed a specialized course on optimizing data science workflows using NVDIA's tools and Technologies.",
+      "Learned techniques to accelerate data processing model training, and deployment without modifying existing codebases.",
+      "✅Gained insights into NVDIA's data scuence acceleration tools and techologies.",
+      "✅Learned methods to optimize data processing and model training workflows for improved perfermance.",
+      "✅Developed strategies for deploying data science models effectively without requiring code changes.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
